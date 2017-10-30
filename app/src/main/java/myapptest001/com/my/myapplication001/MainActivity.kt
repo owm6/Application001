2a65f5f01471bdb52729b43837ec357ec81fe4a4
Leon <lixunpeng@zookingsoft.com>
2017-10-31 00:48:46 +0800

    导入项目

---
+++ app/src/main/java/myapptest001/com/my/myapplication001/MainActivity.kt
@@ -0,0 +1,14 @@
+package myapptest001.com.my.myapplication001
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
