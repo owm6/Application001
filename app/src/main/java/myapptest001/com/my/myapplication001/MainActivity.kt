f827d0daaa8bfa3ae1468b0bead981f16a40890e
Your Name <you@example.com>
2018-02-08 18:10:17 +0800

    dfda fdfadasf

--- app/src/main/java/myapptest001/com/my/myapplication001/MainActivity.kt
+++ app/src/main/java/myapptest001/com/my/myapplication001/MainActivity.kt
@@ -2,13 +2,14 @@ package myapptest001.com.my.myapplication001
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+     //  / val /value: Any = Class s = Log .class;
 
     }
 }
