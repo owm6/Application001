0345259114e3ec9dfaace80d8cc92799a7f749b4
Leon <Ritane@ritekiMacBook-Pro.local>
2018-02-09 10:27:40 +0800

    这不是java语言。。。。

--- app/src/main/java/myapptest001/com/my/myapplication001/MainActivity.kt
+++ app/src/main/java/myapptest001/com/my/myapplication001/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-     //  / val /value: Any = Class s = Log .class;
-
+        //这不是java语言。。。。
     }
 }
