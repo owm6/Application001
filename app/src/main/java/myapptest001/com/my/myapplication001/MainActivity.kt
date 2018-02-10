4d22b09a4ebe1f4d7f0e1cd46bc15d3090952479
Leon <Ritane@ritekiMacBook-Pro.local>
2018-02-10 14:29:29 +0800

    no message

--- app/src/main/java/myapptest001/com/my/myapplication001/MainActivity.kt
+++ app/src/main/java/myapptest001/com/my/myapplication001/MainActivity.kt
@@ -3,6 +3,7 @@ package myapptest001.com.my.myapplication001
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,4 +12,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //这不是java语言。。。。
     }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    class TestClass : Serializable {
+        override fun equals(other: Any?): Boolean {
+            return super.equals(other)
+        }
+    }
+
 }
